6a54d190f32eb652fbbba01b6b703787c6b32f11
AdamWEEFIN <adam.daeira@wee-fin.com>
2025-05-09 16:27:00 +0200

    ajout modèle montant

--- API/models/loader.py
+++ API/models/loader.py
@@ -1,6 +1,7 @@
 import joblib
 
-def load_model(path=r"models_pkls/frequence/xgb_regressor_model.pkl"):
+# Chargement des modèles XGBoost
+def load_model_freq(path=r"models_pkls/frequence/xgb_regressor_model.pkl"):
     return joblib.load(path)
 
 def load_model_montant(path=r"models_pkls/montant/xgb_model (2).pkl"):
