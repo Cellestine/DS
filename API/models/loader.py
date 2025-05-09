41a777b997cbc00eeacd84bf3bb5875de392a3c3
AdamWEEFIN <adam.daeira@wee-fin.com>
2025-05-09 16:16:01 +0200

    loader.py

--- API/models/loader.py
+++ API/models/loader.py
@@ -2,3 +2,6 @@ import joblib
 
 def load_model(path=r"models_pkls/frequence/xgb_regressor_model.pkl"):
     return joblib.load(path)
+
+def load_model_montant(path=r"models_pkls/montant/xgb_model (2).pkl"):
+    return joblib.load(path)
