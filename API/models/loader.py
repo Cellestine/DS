506571aece632a63d75d32513c226768e8d6d0c9
AdamWEEFIN <adam.daeira@wee-fin.com>
2025-05-09 15:53:51 +0200

    loader relative path

--- API/models/loader.py
+++ API/models/loader.py
@@ -1,4 +1,4 @@
 import joblib
 
-def load_model(path=r"C:\Users\CYTech Student\projet\indus_ia\DS\models_pkls\frequence\xgb_regressor_model.pkl"):
+def load_model(path=r"models_pkls/frequence/xgb_regressor_model.pkl"):
     return joblib.load(path)
