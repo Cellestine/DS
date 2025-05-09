1b8d1683ad4896794bcc0bea764c9c15ab9eaf87
Cellestine <celestine_fofana@outlook.com>
2025-05-09 16:46:22 +0200

    [fix] make old a Python package for tests

--- tests/tests_unitaires.py
+++ tests/tests_unitaires.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pandas as pd
 import numpy as np
 from category_encoders import CountEncoder
